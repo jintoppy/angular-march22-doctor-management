94f8c61695833c2319191dcefc7aae36888d7b8d
Jinto Jose <jintoppy@gmail.com>
2022-04-26 15:54:59 +0200

    doctor

--- src/app/features/doctor/components/dashboard/dashboard.component.ts
+++ src/app/features/doctor/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DoctorService } from '../../doctor.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private doctorService: DoctorService) { }
 
   ngOnInit(): void {
+    this.doctorService.getDoctors()
+      .subscribe((val:any) => {
+        console.log(val);
+      })
   }
 
 }
--- src/app/features/doctor/doctor.service.ts
+++ src/app/features/doctor/doctor.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   getDoctors(){
-    return this.http.get('/api/doctors');
+    //interceptor
+    const headers = new HttpHeaders()
+                      .set('Authorization', `Bearer ${this.auth.getAuthToken()}`);
+
+    return this.http.get('/api/doctors', {
+      headers
+    });
   }
 }
